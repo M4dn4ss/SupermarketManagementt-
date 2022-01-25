847585f30ba39c3c14f2501e3a9ddaf22b7f6d2c
M4dn4ss <sura0046@hotmail.com>
2022-01-25 10:08:11 +0300

    Add project files.

---
+++ WebApp/Startup.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Plugins.DataStore.InMemory;
+using UseCases;
+using UseCases.DataStorePluginInterfaces;
+using WebApp.Data;
+
+namespace WebApp
+{
+    public class Startup
+    {
+       public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+            services.AddServerSideBlazor();
+            services.AddSingleton<WeatherForecastService>();
+
+            // Dependecy Injection for In-Memory Data Store
+            services.AddScoped<ICategoryRepository, CategoryInMemoryRepository>();
+
+            // Dependency Injection for Use Cases and Repositories
+            services.AddTransient<IViewCategoriesUseCase, ViewCategoriesUseCase>();
+
+        }
+    }
+}
